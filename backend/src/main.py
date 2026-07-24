aa96fdd7a978a4541cbef7c7d2a2d66fe54ddc3a
Jeswin <jeswinkr7@gmail.com>
2026-07-24 19:32:44 +0300

    Configure production CORS

--- backend/src/main.py
+++ backend/src/main.py
@@ -74,9 +74,14 @@ register_middlewares(app)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        # Local development
         "http://localhost",
         "http://127.0.0.1",
         "http://localhost:5173",
+
+        # Production
+        "https://synapseos.it.com",
+        "https://www.synapseos.it.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
